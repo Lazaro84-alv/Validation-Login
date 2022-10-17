c139883c9d51753607fb35cc36ef5b3c94acf1d4
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-10-17 13:47:30 -0300

    feat(login): implement login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-function App() {
+import { Container, LoginContainer, Column, Spacing, Title } from "./styles";
+import { defaultValues, IFormLogin } from "./types";
+
+const schema = yup
+  .object({
+    email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+    password: yup
+      .string()
+      .min(6, "No minimo 6 caracteres")
+      .required("Campo obrigatório"),
+  })
+  .required();
+
+function Login() {
+  const {
+    control,
+    formState: { errors, isValid },
+  } = useForm<IFormLogin>({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+    defaultValues,
+    reValidateMode: "onChange",
+  });
+
   return (
-    <div className="App">
-      teste
-    </div>
+    <Container>
+      <LoginContainer>
+        <Column>
+          <Title>Login</Title>
+          <Spacing />
+          <Input
+            name="email"
+            placeholder="Email"
+            control={control}
+            errorMessage={errors?.email?.message}
+          />
+          <Spacing />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            control={control}
+            errorMessage={errors?.password?.message}
+          />
+          <Spacing />
+          <Button title="Entrar" />
+        </Column>
+      </LoginContainer>
+    </Container>
   );
-}
+};
 
-export default App;
+export default Login;
